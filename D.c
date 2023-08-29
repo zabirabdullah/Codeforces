259ef42df76c788b7ed3442b1750012f1e1711dc
Zabir Abdullah <91465257+zabirabdullah@users.noreply.github.com>
2023-08-29 19:39:39 +0600

    Create D.c

---
+++ D.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+    long long a, b, c, d, x;
+    scanf("%lld%lld%lld%lld", &a, &b, &c, &d);
+    x = (a*b)-(c*d);
+    printf("Difference = %lld", x);
+
+    return 0;
+}
