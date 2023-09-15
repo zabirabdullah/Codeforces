23310c7ca2c0de0212067e3f09091b2468fb0cdf
Zabir Abdullah <91465257+zabirabdullah@users.noreply.github.com>
2023-09-15 22:33:43 +0600

    Create O.c

---
+++ O.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+    int n, i, j;
+    scanf("%d", &n);
+    for(i=1; i<=n; i++)
+    {
+        j = 1;
+        while(j<=i)
+        {
+            printf("*");
+            j++;
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
