d106c96319f769eb73096fd5b99239c8d19c1a71
Zabir Abdullah <91465257+zabirabdullah@users.noreply.github.com>
2023-09-17 17:32:33 +0600

    Create W.c

---
+++ W.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int main(){
+    int n, i, j;
+    scanf("%d", &n);
+
+    for(i=n; i>0; i--)
+    {
+        for(j=1; j<i; j++)
+        {
+            printf(" ");
+        }
+        for(j=i; j<=n; j++)
+        {
+            printf("*");
+        }
+        for(j=i; j<n; j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    for(i=0; i<n; i++)
+    {
+        for(j=n-i; j<n; j++)
+        {
+            printf(" ");
+        }
+        for(j=i; j<n; j++)
+        {
+            printf("*");
+        }
+        for(j=i+1; j<n; j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+
+    }
+
+    return 0;
+}
